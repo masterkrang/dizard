b390345efcf301275e35eda0302fb8dcebde2ca8
Kurt Braget <kurtbraget@gmail.com>
2013-12-22 20:31:47 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dizard::Application.config.secret_token = '6306382a595a8a45c1c0dbf958dcd41e023ac70c02f32b548186d0aad8b5d158e035deaffd3a09db2f1ec2ce3890ae18fd813b5428cb080e08504ef65e175ac8'
